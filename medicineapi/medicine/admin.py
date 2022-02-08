1e23cf3aa874851cf222395e943193166b118f62
ÐNikita Kurabtsev <nick3500hunna@gmail.com>
2022-02-08 20:48:43 +0200

    register models in admin.py

--- medicineapi/medicine/admin.py
+++ medicineapi/medicine/admin.py
@@ -1,7 +1,24 @@
-from dataclasses import field
 from django.contrib import admin
 from .models import Company, Medicine
 
 
-admin.site.register(Company)
-admin.site.register(Medicine)
+# admin.site.register(Company)
+# admin.site.register(Medicine)
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'created', 'slug')
+    list_filter = ('name', 'created')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Medicine)
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'medicine_category',
+        'company', 'description',
+        'expiration_date', 'slug'
+    )
+    list_filter = ('name', 'medicine_category', 'company', 'description')
+    search_fields = ('name', 'description')
+    ordering = ('name', 'company')
