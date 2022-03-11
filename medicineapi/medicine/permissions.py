23c6feb0321a814b248c45ceb3891402cce2922f
ÐNikita Kurabtsev <nick3500hunna@gmail.com>
2022-03-11 12:50:15 +0200

    refactor

--- medicineapi/medicine/permissions.py
+++ medicineapi/medicine/permissions.py
@@ -1,24 +1,10 @@
 from rest_framework import permissions
 
-ACTION_SAFE_METHODS = [
-    'list',
-    'retrieve'
-]
-
-
-# class IsOwnerOrReadOnly(permissions.BasePermission):
-#     """Allow company owners to edit their medicines."""
-    
-#     def has_object_permission(self, request, view, obj):
-#         if request.method in permissions.SAFE_METHODS:
-#             return True
-#         return obj.company.owner == request.user
-
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """Allow company owners to edit their medicines."""
     def has_object_permission(self, request, view, obj):
-        if view.action in ACTION_SAFE_METHODS:
+        if view.action in ('list', 'retrieve'):
             return True
 
         return obj.company.owner == request.user
