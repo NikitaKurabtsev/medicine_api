0dcd1ef83e336c8dc83bb83f72fc642da24f2fdb
NikitaKurabtsev <nick3500hunna@gmail.com>
2022-06-29 10:11:18 +0300

    add view tests

--- medicineapi/medicineapi/settings.py
+++ medicineapi/medicineapi/settings.py
@@ -129,8 +129,15 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ],
+# }
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
